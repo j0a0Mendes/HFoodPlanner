703a66ab79dc36de693063cb86808c8540c22983
Joãooo <mm2000_@hotmail.com>
2023-12-08 14:43:41 +0100

    add: category onclick leads to category page

--- app/src/main/java/com/example/hfoodplanner/ui/category_selected/CategorySelectedActivity.java
+++ app/src/main/java/com/example/hfoodplanner/ui/category_selected/CategorySelectedActivity.java
@@ -1,6 +1,36 @@
 package com.example.hfoodplanner.ui.category_selected;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.hfoodplanner.R;
+import com.example.hfoodplanner.databinding.ActivityCategorySelectedBinding;
+import com.example.hfoodplanner.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class CategorySelectedActivity extends AppCompatActivity {
+
+    private String category;
+    private ActivityCategorySelectedBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Bundle extras = getIntent().getExtras();
+
+        if(extras != null){
+            category = extras.getString("category");
+        }
+
+        binding = ActivityCategorySelectedBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ((TextView) findViewById(R.id.text_category_selected)).setText(category);
+    }
 }
--- app/src/main/java/com/example/hfoodplanner/ui/dish_categories/DishCategoriesFragment.java
+++ app/src/main/java/com/example/hfoodplanner/ui/dish_categories/DishCategoriesFragment.java
@@ -1,21 +1,32 @@
 package com.example.hfoodplanner.ui.dish_categories;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 
+import com.example.hfoodplanner.R;
 import com.example.hfoodplanner.databinding.FragmentDishCategoriesBinding;
+import com.example.hfoodplanner.ui.category_selected.CategorySelectedActivity;
 
-public class DishCategoriesFragment extends Fragment{
+public class DishCategoriesFragment extends Fragment implements View.OnClickListener{
 
     private FragmentDishCategoriesBinding binding;
+    private CardView meat, fish, vegetarian, vegan, snacks, deserts;
+
+    public DishCategoriesFragment() {
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,13 +36,54 @@ public class DishCategoriesFragment extends Fragment{
         binding = FragmentDishCategoriesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        meat = root.findViewById(R.id.meat_category);
+        fish = root.findViewById(R.id.fish_category);
+        vegetarian = root.findViewById(R.id.vegetarian_category);
+        vegan = root.findViewById(R.id.vegan_category);
+        snacks = root.findViewById(R.id.snacks_category);
+        deserts = root.findViewById(R.id.deserts_category);
+
+        meat.setOnClickListener(this);
+        fish.setOnClickListener(this);
+        vegetarian.setOnClickListener(this);
+        vegan.setOnClickListener(this);
+        snacks.setOnClickListener(this);
+        deserts.setOnClickListener(this);
+
 
         return root;
     }
+
+
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
-}
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(getActivity(), CategorySelectedActivity.class);
+
+        String category_selected = "";
+
+        if(view.getId() == R.id.meat_category){
+            category_selected = "Meat";
+        } else if (view.getId() == R.id.fish_category) {
+            category_selected = "Fish";
+        }else if (view.getId() == R.id.vegetarian_category) {
+            category_selected = "Vegetarian";
+        }else if (view.getId() == R.id.vegan_category) {
+            category_selected = "Vegan";
+        }else if (view.getId() == R.id.snacks_category) {
+            category_selected = "Snacks";
+        }else if (view.getId() == R.id.deserts_category) {
+            category_selected = "Deserts";
+        }
+
+        intent.putExtra("category", category_selected);
+        startActivity(intent);
+        }
+    }
+
