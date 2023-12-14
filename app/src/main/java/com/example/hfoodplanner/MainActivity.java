b695cb12eeef48c4d2289c8cdbd44466b62a76dd
Joãooo <mm2000_@hotmail.com>
2023-12-14 12:31:30 +0100

    add: calendar fragment layout done
    - Calendar fragment layout done (sketch)
    - App top bar in main activity removed

--- app/src/main/java/com/example/hfoodplanner/MainActivity.java
+++ app/src/main/java/com/example/hfoodplanner/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.hfoodplanner;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -19,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
