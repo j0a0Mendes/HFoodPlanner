3008b9bf26b51a2b8cb17d29a334bfaea97ef4f7
Joãooo <mm2000_@hotmail.com>
2024-01-15 22:58:28 +0100

    add: week mechanism added
    - week days are updated with the current week days
    - current week day turns green

--- app/src/main/java/com/example/hfoodplanner/MainActivity.java
+++ app/src/main/java/com/example/hfoodplanner/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.hfoodplanner;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -14,9 +17,16 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.hfoodplanner.databinding.ActivityMainBinding;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private static ArrayList<Integer> weekDays;
+
+    DayOfWeek currentDayOfWeek;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +48,45 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        //create list of current week days
+        weekDays = new ArrayList<Integer>();
+
+        //Get days of the week
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            setCurrentDayAndWeekDays();
+        }
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void setCurrentDayAndWeekDays() {
+        // Get the current date
+        LocalDate currentDate = LocalDate.now();
+
+        // Find the most recent Monday
+        LocalDate monday = currentDate.with(DayOfWeek.MONDAY);
+
+        // Get the current day of the week
+        currentDayOfWeek = currentDate.getDayOfWeek();
+        //Log.d("DATE_TIME", "Current Day of the Week: " + currentDayOfWeek);
+
+        // Get the current day of the month
+        int currentDayOfMonth = currentDate.getDayOfMonth();
+        //Log.d("DATE_TIME", "Current Day of the Month: " + currentDayOfMonth);
+
+        // Get the days from the past Monday until the next Sunday
+        for (int i = 0; i <= DayOfWeek.SUNDAY.getValue() - DayOfWeek.MONDAY.getValue(); i++) {
+            LocalDate dayOfWeek = monday.plusDays(i);
+            int dayOfMonth = dayOfWeek.getDayOfMonth();
+            //Log.d("DATE_TIME", "Day " + (i + 1) + ": " + dayOfWeek.getDayOfWeek() + ", Day of the Month: " + dayOfMonth);
+            weekDays.add(dayOfMonth);
+        }
+    }
+
+    public ArrayList<Integer> getWeekDays(){
+        return weekDays;
+    }
+
+    public String getCurrentWeekDay(){ return currentDayOfWeek.toString();}
 
 }
--- app/src/main/java/com/example/hfoodplanner/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/example/hfoodplanner/ui/calendar/CalendarFragment.java
@@ -1,21 +1,40 @@
 package com.example.hfoodplanner.ui.calendar;
 
+import static android.R.color.holo_green_light;
+
+import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.hfoodplanner.MainActivity;
+import com.example.hfoodplanner.R;
 import com.example.hfoodplanner.databinding.FragmentCalendarBinding;
 import com.example.hfoodplanner.ui.calendar.CalendarViewModel;
+
+import java.time.LocalDate;
+import java.time.DayOfWeek;
+import java.util.ArrayList;
 
 public class CalendarFragment extends Fragment {
 
     private FragmentCalendarBinding binding;
 
+    private MainActivity activity;
+    private int currentWeekIndex = 0;
+    private ArrayList<Integer> weekDays;
+
+    @SuppressLint("SetTextI18n")
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         CalendarViewModel dishCategoriesViewModel =
@@ -23,6 +42,15 @@ public class CalendarFragment extends Fragment {
 
         binding = FragmentCalendarBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        activity = (MainActivity)getActivity();
+
+        //get weekdays info from main activity
+        weekDays = activity.getWeekDays();
+
+        Log.d("Week", String.valueOf(weekDays));
+
+        setUpWeekMechanism(root);
 
 
         return root;
@@ -32,5 +60,58 @@ public class CalendarFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public void setUpWeekMechanism(View root) {
+        //set the week days of the month
+        TextView tv_firstDay = root.findViewById(R.id.month_day_one);
+        tv_firstDay.setText(weekDays.get(0).toString());
+        TextView tv_secondDay = root.findViewById(R.id.month_day_two);
+        tv_secondDay.setText(weekDays.get(1).toString());
+        TextView tv_thirdDay = root.findViewById(R.id.month_day_three);
+        tv_thirdDay.setText(weekDays.get(2).toString());
+        TextView tv_forthDay = root.findViewById(R.id.month_day_four);
+        tv_forthDay.setText(weekDays.get(3).toString());
+        TextView tv_fifthDay = root.findViewById(R.id.month_day_five);
+        tv_fifthDay.setText(weekDays.get(4).toString());
+        TextView tv_sixthDay = root.findViewById(R.id.month_day_six);
+        tv_sixthDay.setText(weekDays.get(5).toString());
+        TextView tv_seventhDay = root.findViewById(R.id.month_day_seven);
+        tv_seventhDay.setText(weekDays.get(6).toString());
+
+        CardView cv_Mon = root.findViewById(R.id.day_one);
+        CardView cv_Tue = root.findViewById(R.id.day_two);
+        CardView cv_Wed = root.findViewById(R.id.day_three);
+        CardView cv_Thu = root.findViewById(R.id.day_four);
+        CardView cv_Fri = root.findViewById(R.id.day_five);
+        CardView cv_Sat = root.findViewById(R.id.day_six);
+        CardView cv_Sun = root.findViewById(R.id.day_seven);
+
+
+        int green = getResources().getColor(holo_green_light);
+
+        switch (activity.getCurrentWeekDay()){
+            case "MONDAY":
+                cv_Mon.setCardBackgroundColor(green);
+                break;
+            case "TUESDAY":
+                cv_Tue.setCardBackgroundColor(green);
+                break;
+            case "WEDNESDAY":
+                cv_Wed.setCardBackgroundColor(green);
+                break;
+            case "THURSDAY":
+                cv_Thu.setCardBackgroundColor(green);
+                break;
+            case "FRIDAY":
+                cv_Fri.setCardBackgroundColor(green);
+                break;
+            case "SATURDAY":
+                cv_Sat.setCardBackgroundColor(green);
+                break;
+            case "SUNDAY":
+                cv_Sun.setCardBackgroundColor(green);
+                break;
+        }
     }
 }
